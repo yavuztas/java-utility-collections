0aeb53c79fed4ce4d59bb983033a7758490b5e66
Yavuz Tas <yavuzslmtas@gmail.com>
2019-08-04 13:21:04 +0200

    update formatting

--- src/main/java/dev/yavuztas/utils/collections/UtilsForCollections.java
+++ src/main/java/dev/yavuztas/utils/collections/UtilsForCollections.java
@@ -17,92 +17,124 @@ import java.util.stream.Collectors;
  */
 public class UtilsForCollections {
 
-	private UtilsForCollections() {
+    private UtilsForCollections() {
 
-	}
+    }
 
-	public static <T> List<T> toList(T[] array) {
-		return Arrays.stream(array).collect(Collectors.toList());
-	}
+    public static <T> List<T> toList(T[] array) {
+        return Arrays.stream(array)
+            .collect(Collectors.toList());
+    }
 
-	public static List<Integer> toList(int[] array) {
-		return Arrays.stream(array).boxed().collect(Collectors.toList());
-	}
+    public static List<Integer> toList(int[] array) {
+        return Arrays.stream(array)
+            .boxed()
+            .collect(Collectors.toList());
+    }
 
-	public static List<Long> toList(long[] array) {
-		return Arrays.stream(array).boxed().collect(Collectors.toList());
-	}
+    public static List<Long> toList(long[] array) {
+        return Arrays.stream(array)
+            .boxed()
+            .collect(Collectors.toList());
+    }
 
-	public static List<Double> toList(double[] array) {
-		return Arrays.stream(array).boxed().collect(Collectors.toList());
-	}
+    public static List<Double> toList(double[] array) {
+        return Arrays.stream(array)
+            .boxed()
+            .collect(Collectors.toList());
+    }
 
-	public static <T> Set<T> toSet(T[] array) {
-		return UtilsForCollections.toSet(array, false);
-	}
+    public static <T> Set<T> toSet(T[] array) {
+        return UtilsForCollections.toSet(array, false);
+    }
 
-	public static <T> Set<T> toSet(T[] array, boolean preserveOrder) {
-		if (preserveOrder) {
-			return Arrays.stream(array).collect(Collectors.toCollection(LinkedHashSet::new));
-		}
-		return Arrays.stream(array).collect(Collectors.toSet());
-	}
+    public static <T> Set<T> toSet(T[] array, boolean preserveOrder) {
+        if (preserveOrder) {
+            return Arrays.stream(array)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        }
+        return Arrays.stream(array)
+            .collect(Collectors.toSet());
+    }
 
-	public static Set<Integer> toSet(int[] array) {
-		return Arrays.stream(array).boxed().collect(Collectors.toSet());
-	}
+    public static Set<Integer> toSet(int[] array) {
+        return Arrays.stream(array)
+            .boxed()
+            .collect(Collectors.toSet());
+    }
 
-	public static Set<Long> toSet(long[] array) {
-		return Arrays.stream(array).boxed().collect(Collectors.toSet());
-	}
+    public static Set<Long> toSet(long[] array) {
+        return Arrays.stream(array)
+            .boxed()
+            .collect(Collectors.toSet());
+    }
 
-	public static Set<Double> toSet(double[] array) {
-		return Arrays.stream(array).boxed().collect(Collectors.toSet());
-	}
+    public static Set<Double> toSet(double[] array) {
+        return Arrays.stream(array)
+            .boxed()
+            .collect(Collectors.toSet());
+    }
 
-	public static <T> List<T> toList(Collection<T> collection) {
-		return collection.stream().collect(Collectors.toList());
-	}
+    public static <T> List<T> toList(Collection<T> collection) {
+        return collection.stream()
+            .collect(Collectors.toList());
+    }
 
-	public static <T> Set<T> toSet(Collection<T> collection) {
-		return UtilsForCollections.toSet(collection, false);
-	}
+    public static <T> Set<T> toSet(Collection<T> collection) {
+        return UtilsForCollections.toSet(collection, false);
+    }
 
-	public static <T> Set<T> toSet(Collection<T> collection, boolean preserveOrder) {
-		if (preserveOrder) {
-			return collection.stream().collect(Collectors.toCollection(LinkedHashSet::new));
-		}
-		return collection.stream().collect(Collectors.toSet());
-	}
+    public static <T> Set<T> toSet(Collection<T> collection, boolean preserveOrder) {
+        if (preserveOrder) {
+            return collection.stream()
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        }
+        return collection.stream()
+            .collect(Collectors.toSet());
+    }
 
-	public static <T> List<T> filterArray(T[] array, Predicate<? super T> predicate) {
-		return Arrays.stream(array).filter(predicate).collect(Collectors.toCollection(ArrayList::new));
-	}
+    public static <T> List<T> filterArray(T[] array, Predicate<? super T> predicate) {
+        return Arrays.stream(array)
+            .filter(predicate)
+            .collect(Collectors.toCollection(ArrayList::new));
+    }
 
-	public static <T> List<T> filterCollection(Collection<T> collection, Predicate<? super T> predicate) {
-		return collection.stream().filter(predicate).collect(Collectors.toCollection(ArrayList::new));
-	}
+    public static <T> List<T> filterCollection(Collection<T> collection, Predicate<? super T> predicate) {
+        return collection.stream()
+            .filter(predicate)
+            .collect(Collectors.toCollection(ArrayList::new));
+    }
 
-	public static <T> String join(T[] array, String separator) {
-		return UtilsForCollections.join(array, separator, 0);
-	}
+    public static <T> String join(T[] array, String separator) {
+        return UtilsForCollections.join(array, separator, 0);
+    }
 
-	public static <T> String join(T[] array, String separator, int limit) {
-		if (limit > 0 && array.length > limit) {
-			return Arrays.stream(array).limit(limit).map(String::valueOf).collect(Collectors.joining(separator)) + separator + "...";
-		}
-		return Arrays.stream(array).map(String::valueOf).collect(Collectors.joining(separator));
-	}
+    public static <T> String join(T[] array, String separator, int limit) {
+        if (limit > 0 && array.length > limit) {
+            return Arrays.stream(array)
+                .limit(limit)
+                .map(String::valueOf)
+                .collect(Collectors.joining(separator)) + separator + "...";
+        }
+        return Arrays.stream(array)
+            .map(String::valueOf)
+            .collect(Collectors.joining(separator));
+    }
 
-	public static <T> String join(Collection<T> collection, String separator) {
-		return UtilsForCollections.join(collection, separator, 0);
-	}
+    public static <T> String join(Collection<T> collection, String separator) {
+        return UtilsForCollections.join(collection, separator, 0);
+    }
 
-	public static <T> String join(Collection<T> collection, String separator, int limit) {
-		if (limit > 0 && collection.size() > limit) {
-			return collection.stream().limit(limit).map(String::valueOf).collect(Collectors.joining(separator)) + separator + "...";
-		}
-		return collection.stream().map(String::valueOf).collect(Collectors.joining(separator));
-	}
+    public static <T> String join(Collection<T> collection, String separator, int limit) {
+        if (limit > 0 && collection.size() > limit) {
+            return collection.stream()
+                .limit(limit)
+                .map(String::valueOf)
+                .collect(Collectors.joining(separator)) + separator + "...";
+        }
+        return collection.stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining(separator));
+    }
 
 }
--- src/test/java/dev/yavuztas/utils/test/UtilsForCollectionsTest.java
+++ src/test/java/dev/yavuztas/utils/test/UtilsForCollectionsTest.java
@@ -19,228 +19,232 @@ import junit.framework.Assert;
  */
 public class UtilsForCollectionsTest {
 
-	@Test
-	public void arrayToListTest() {
+    @Test
+    public void arrayToListTest() {
 
-		String[] array = new String[] { "apple", "banana", "orange" };
+        String[] array = new String[] { "apple", "banana", "orange" };
 
-		List<String> list = UtilsForCollections.toList(array);
-		Assert.assertEquals(array.length, list.size());
+        List<String> list = UtilsForCollections.toList(array);
+        Assert.assertEquals(array.length, list.size());
 
-		for (int i = 0; i < array.length; i++) {
-			Assert.assertEquals(array[i], list.get(i));
-		}
+        for (int i = 0; i < array.length; i++) {
+            Assert.assertEquals(array[i], list.get(i));
+        }
 
-	}
+    }
 
-	@Test
-	public void primitiveIntArrayToListTest() {
+    @Test
+    public void primitiveIntArrayToListTest() {
 
-		int[] intArray = new int[] { 1, 2, 3 };
+        int[] intArray = new int[] { 1, 2, 3 };
 
-		List<Integer> list = UtilsForCollections.toList(intArray);
-		Assert.assertEquals(intArray.length, list.size());
+        List<Integer> list = UtilsForCollections.toList(intArray);
+        Assert.assertEquals(intArray.length, list.size());
 
-		for (int i = 0; i < intArray.length; i++) {
-			Assert.assertEquals(intArray[i], list.get(i).intValue());
-		}
+        for (int i = 0; i < intArray.length; i++) {
+            Assert.assertEquals(intArray[i], list.get(i)
+                .intValue());
+        }
 
-	}
+    }
 
-	@Test
-	public void primitiveLongArrayToListTest() {
+    @Test
+    public void primitiveLongArrayToListTest() {
 
-		long[] array = new long[] { 1, 3, 5, 6, 7 };
+        long[] array = new long[] { 1, 3, 5, 6, 7 };
 
-		List<Long> list = UtilsForCollections.toList(array);
-		Assert.assertEquals(array.length, list.size());
+        List<Long> list = UtilsForCollections.toList(array);
+        Assert.assertEquals(array.length, list.size());
 
-		for (int i = 0; i < array.length; i++) {
-			Assert.assertEquals(array[i], list.get(i).longValue());
-		}
+        for (int i = 0; i < array.length; i++) {
+            Assert.assertEquals(array[i], list.get(i)
+                .longValue());
+        }
 
-	}
+    }
 
-	@Test
-	public void primitiveDoubleArrayToListTest() {
+    @Test
+    public void primitiveDoubleArrayToListTest() {
 
-		double[] array = new double[] { 1.0, 3.0, 5.1, 6.2, 7.3 };
+        double[] array = new double[] { 1.0, 3.0, 5.1, 6.2, 7.3 };
 
-		List<Double> list = UtilsForCollections.toList(array);
-		Assert.assertEquals(array.length, list.size());
+        List<Double> list = UtilsForCollections.toList(array);
+        Assert.assertEquals(array.length, list.size());
 
-		for (int i = 0; i < array.length; i++) {
-			Assert.assertEquals(array[i], list.get(i).doubleValue());
-		}
+        for (int i = 0; i < array.length; i++) {
+            Assert.assertEquals(array[i], list.get(i)
+                .doubleValue());
+        }
 
-	}
+    }
 
-	@Test
-	public void arrayToSetTest() {
+    @Test
+    public void arrayToSetTest() {
 
-		String[] array = new String[] { "apple", "banana", "banana", "orange" };
+        String[] array = new String[] { "apple", "banana", "banana", "orange" };
 
-		Set<String> set = UtilsForCollections.toSet(array);
-		Assert.assertEquals(array.length - 1, set.size());
+        Set<String> set = UtilsForCollections.toSet(array);
+        Assert.assertEquals(array.length - 1, set.size());
 
-		Iterator<String> iterator = set.iterator();
-		while (iterator.hasNext()) {
-			String element = (String) iterator.next();
-			int found = Arrays.binarySearch(array, element);
-			Assert.assertTrue(found > -1);
-			Assert.assertEquals(array[found], element);
-		}
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            String element = (String) iterator.next();
+            int found = Arrays.binarySearch(array, element);
+            Assert.assertTrue(found > -1);
+            Assert.assertEquals(array[found], element);
+        }
 
-	}
+    }
 
-	@Test
-	public void primitiveIntArrayToSetTest() {
+    @Test
+    public void primitiveIntArrayToSetTest() {
 
-		int[] array = new int[] { 1, 2, 2, 3 };
+        int[] array = new int[] { 1, 2, 2, 3 };
 
-		Set<Integer> set = UtilsForCollections.toSet(array);
-		Assert.assertEquals(array.length - 1, set.size());
+        Set<Integer> set = UtilsForCollections.toSet(array);
+        Assert.assertEquals(array.length - 1, set.size());
 
-		Iterator<Integer> iterator = set.iterator();
-		while (iterator.hasNext()) {
-			Integer element = iterator.next();
-			int found = Arrays.binarySearch(array, element);
-			Assert.assertTrue(found > -1);
-			Assert.assertEquals(array[found], element.intValue());
-		}
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            int found = Arrays.binarySearch(array, element);
+            Assert.assertTrue(found > -1);
+            Assert.assertEquals(array[found], element.intValue());
+        }
 
-	}
+    }
 
-	@Test
-	public void primitiveLongArrayToSetTest() {
+    @Test
+    public void primitiveLongArrayToSetTest() {
 
-		long[] array = new long[] { 1, 3, 5, 6, 7 };
+        long[] array = new long[] { 1, 3, 5, 6, 7 };
 
-		Set<Long> set = UtilsForCollections.toSet(array);
-		Assert.assertEquals(array.length, set.size());
+        Set<Long> set = UtilsForCollections.toSet(array);
+        Assert.assertEquals(array.length, set.size());
 
-		Iterator<Long> iterator = set.iterator();
-		while (iterator.hasNext()) {
-			Long element = iterator.next();
-			int found = Arrays.binarySearch(array, element);
-			Assert.assertTrue(found > -1);
-			Assert.assertEquals(array[found], element.longValue());
-		}
+        Iterator<Long> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Long element = iterator.next();
+            int found = Arrays.binarySearch(array, element);
+            Assert.assertTrue(found > -1);
+            Assert.assertEquals(array[found], element.longValue());
+        }
 
-	}
+    }
 
-	@Test
-	public void primitiveDoubleArrayToSetTest() {
+    @Test
+    public void primitiveDoubleArrayToSetTest() {
 
-		double[] array = new double[] { 1.0, 3.0, 5.1, 6.2, 7.3 };
+        double[] array = new double[] { 1.0, 3.0, 5.1, 6.2, 7.3 };
 
-		Set<Double> set = UtilsForCollections.toSet(array);
-		Assert.assertEquals(array.length, set.size());
+        Set<Double> set = UtilsForCollections.toSet(array);
+        Assert.assertEquals(array.length, set.size());
 
-		Iterator<Double> iterator = set.iterator();
-		while (iterator.hasNext()) {
-			Double element = iterator.next();
-			int found = Arrays.binarySearch(array, element);
-			Assert.assertTrue(found > -1);
-			Assert.assertEquals(array[found], element.doubleValue());
-		}
+        Iterator<Double> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Double element = iterator.next();
+            int found = Arrays.binarySearch(array, element);
+            Assert.assertTrue(found > -1);
+            Assert.assertEquals(array[found], element.doubleValue());
+        }
 
-	}
+    }
 
-	@Test
-	public void arrayToSetPreserverOrderTest() {
+    @Test
+    public void arrayToSetPreserverOrderTest() {
 
-		String[] array = new String[] { "apple", "banana", "avocado", "orange" };
+        String[] array = new String[] { "apple", "banana", "avocado", "orange" };
 
-		Set<String> set = UtilsForCollections.toSet(array, true);
-		Assert.assertEquals(array.length, set.size());
+        Set<String> set = UtilsForCollections.toSet(array, true);
+        Assert.assertEquals(array.length, set.size());
 
-		int i = 0;
-		Iterator<String> iterator = set.iterator();
-		while (iterator.hasNext()) {
-			Assert.assertEquals(array[i++], (String) iterator.next());
-		}
-	}
+        int i = 0;
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Assert.assertEquals(array[i++], (String) iterator.next());
+        }
+    }
 
-	@Test
-	public void listToSetTest() {
+    @Test
+    public void listToSetTest() {
 
-		List<String> list = Arrays.asList(new String[] { "apple", "banana", "banana", "orange" });
-		Set<String> set = UtilsForCollections.toSet(list);
+        List<String> list = Arrays.asList(new String[] { "apple", "banana", "banana", "orange" });
+        Set<String> set = UtilsForCollections.toSet(list);
 
-		Assert.assertEquals(list.size() - 1, set.size());
+        Assert.assertEquals(list.size() - 1, set.size());
 
-		for (String element : set) {
-			Assert.assertTrue(list.contains(element));
-		}
+        for (String element : set) {
+            Assert.assertTrue(list.contains(element));
+        }
 
-	}
+    }
 
-	@Test
-	public void setToListTest() {
+    @Test
+    public void setToListTest() {
 
-		Set<String> set = Arrays.stream(new String[] { "apple", "banana", "avocado", "orange" }).collect(Collectors.toSet());
-		List<String> list = UtilsForCollections.toList(set);
+        Set<String> set = Arrays.stream(new String[] { "apple", "banana", "avocado", "orange" })
+            .collect(Collectors.toSet());
+        List<String> list = UtilsForCollections.toList(set);
 
-		Assert.assertEquals(set.size(), list.size());
+        Assert.assertEquals(set.size(), list.size());
 
-		for (String element : list) {
-			Assert.assertTrue(set.contains(element));
-		}
+        for (String element : list) {
+            Assert.assertTrue(set.contains(element));
+        }
 
-	}
+    }
 
-	@Test
-	public void listToSetPreserverOrderTest() {
+    @Test
+    public void listToSetPreserverOrderTest() {
 
-		List<String> list = Arrays.asList(new String[] { "apple", "banana", "avocado", "orange" });
-		Set<String> set = UtilsForCollections.toSet(list, true);
+        List<String> list = Arrays.asList(new String[] { "apple", "banana", "avocado", "orange" });
+        Set<String> set = UtilsForCollections.toSet(list, true);
 
-		Assert.assertEquals(list.size(), set.size());
+        Assert.assertEquals(list.size(), set.size());
 
-		int i = 0;
-		for (String element : set) {
-			Assert.assertEquals(list.get(i++), element);
-		}
+        int i = 0;
+        for (String element : set) {
+            Assert.assertEquals(list.get(i++), element);
+        }
 
-	}
+    }
 
-	@Test
-	public void filterArrayTest() {
+    @Test
+    public void filterArrayTest() {
 
-		String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
-		List<String> filtered = UtilsForCollections.filterArray(array, v -> v.startsWith("a"));
+        String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
+        List<String> filtered = UtilsForCollections.filterArray(array, v -> v.startsWith("a"));
 
-		Assert.assertEquals(filtered.size(), 2);
-		Assert.assertEquals(filtered.get(0), "apple");
-		Assert.assertEquals(filtered.get(1), "avocado");
-	}
+        Assert.assertEquals(filtered.size(), 2);
+        Assert.assertEquals(filtered.get(0), "apple");
+        Assert.assertEquals(filtered.get(1), "avocado");
+    }
 
-	@Test
-	public void filterCollectionTest() {
+    @Test
+    public void filterCollectionTest() {
 
-		String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
-		List<String> filtered = UtilsForCollections.filterCollection(Arrays.asList(array), v -> v.startsWith("a"));
+        String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
+        List<String> filtered = UtilsForCollections.filterCollection(Arrays.asList(array), v -> v.startsWith("a"));
 
-		Assert.assertEquals(filtered.size(), 2);
-		Assert.assertEquals(filtered.get(0), "apple");
-		Assert.assertEquals(filtered.get(1), "avocado");
-	}
+        Assert.assertEquals(filtered.size(), 2);
+        Assert.assertEquals(filtered.get(0), "apple");
+        Assert.assertEquals(filtered.get(1), "avocado");
+    }
 
-	@Test
-	public void arrayToStringTest() {
+    @Test
+    public void arrayToStringTest() {
 
-		String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
-		String string = UtilsForCollections.join(array, ", ", 3);
-		Assert.assertEquals("apple, banana, orange, ...", string);
-	}
+        String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
+        String string = UtilsForCollections.join(array, ", ", 3);
+        Assert.assertEquals("apple, banana, orange, ...", string);
+    }
 
-	@Test
-	public void collectionToStringTest() {
+    @Test
+    public void collectionToStringTest() {
 
-		String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
-		String string = UtilsForCollections.join(Arrays.asList(array), ", ", 10);
-		Assert.assertEquals("apple, banana, orange, avocado, mango", string);
-	}
+        String[] array = new String[] { "apple", "banana", "orange", "avocado", "mango" };
+        String string = UtilsForCollections.join(Arrays.asList(array), ", ", 10);
+        Assert.assertEquals("apple, banana, orange, avocado, mango", string);
+    }
 
 }
